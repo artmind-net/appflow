2c4032e9857a8a3f1b00bf6fb4d9980b3cddf66d
anegara <6215896+anegara@users.noreply.github.com>
2020-06-12 14:12:43 +0300

    add CancellationToken propagation

---
+++ src/ArtMind.AppFlow.Abstractions/Enums.cs
@@ -0,0 +1,14 @@
+﻿namespace ArtMind.AppFlow
+{
+    public enum CancellationTokenPropagation
+    {
+        /// <summary>
+        /// CancellationToken request is checked before each new cycle initiation.
+        /// </summary>
+        AtFlowCycle,
+        /// <summary>
+        /// CancellationToken request is checked before each new cycle initiation and before each flow-task initiation.
+        /// </summary>
+        InFlowDepth
+    }
+}
